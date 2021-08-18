f2dd7e415c23f4e89322742e7326d163fb4717ae
hashrock <hashrock@users.noreply.github.com>
2021-08-18 23:01:31 +0900

    feat: add alive message

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
+let timer
+
 new Vue({
   el: "#app",
   data() {
     return {
       message: "Hello",
       status: "",
+      user: {
+        id: window.crypto.randomUUID(),
+        name: "anonymous",
+      },
+      users: {}
     }
   },
   methods: {
-    send(){
+    send() {
       fetch("/api/send", {
         method: "POST",
-        body: JSON.stringify({ user: "user", body: "message" }),
+        body: JSON.stringify({ user: this.user, type: "message", body: "Hello" }),
       })
+    },
+    async sendAlive() {
+      await fetch("/api/send", {
+        method: "POST",
+        body: JSON.stringify({ user: this.user, type: "alive", body: "" }),
+      })
+      // console.log(await res.json())
     }
   },
+  beforeDestroy() {
+    clearInterval(timer)
+  },
   mounted() {
+    timer = setInterval(() => {
+      if (this.status === "CONNECTED") {
+        this.sendAlive()
+      }
+    }, 2000);
+
     const events = new EventSource("/api/listen");
     events.addEventListener("open", () => this.status = "CONNECTED");
     events.addEventListener("error", () => {
@@ -31,8 +54,31 @@ new Vue({
       }
     });
     events.addEventListener("message", (e) => {
-      console.log(e.data)
+      const msg = JSON.parse(e.data)
+      if(msg.type === "alive") {
+        this.$set(this.users, msg.user.id, msg.user)
+        // this.users[msg.user.id] = msg.user
 
+        // this.usersの中で古いデータを削除
+        Object.keys(this.users).forEach(id => {
+          // 1分以上古いデータを削除
+          if (this.users[id].ts < Date.now() - 10 * 1000) {
+            this.$delete(this.users, id)
+          }
+        })
+
+        // if (!this.users.(msg.user.id)) {
+        //   this.$set(this.users, msg.user.id, msg.user)
+        //   // this.users.set(user.id, user);
+        // }
+        // for (const [id, user] of this.users.entries()) {
+        //   if (user.ts < new Date().getTime() - 1000 * 60 * 60) {
+        //     this.$delete(this.users, id)
+        //     // users.delete(id);
+        //   }
+        // }
+      }
+      console.log(e.data)
       // addMessage(JSON.parse(e.data));
     });
   }
--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
-import { serve, serveStatic } from "https://deno.land/x/sift@0.3.5/mod.ts";
+import {
+  serve,
+  serveStatic,
+  json
+} from "https://deno.land/x/sift@0.3.5/mod.ts";
+
 export interface Message {
   id: string;
   ts: string;
-  user: string;
+  user: User;
   body: string;
+  type: string;
 }
 import { generate as generateUUID } from "https://deno.land/std@0.98.0/uuid/v4.ts";
+
+const users = new Map<string, User>();
+
+interface User {
+  id: string;
+  name: string;
+  ts: number;
+}
 
 serve({
   "/": serveStatic("index.html", { baseUrl: import.meta.url }),
@@ -14,16 +28,16 @@ serve({
   "/api/send": async (req) => {
     const msg = await req.json();
 
-    const user = msg["user"];
-    if (typeof user !== "string") {
-      return new Response("invalid user", { status: 400 });
-    }
+    const user: User = msg["user"];
 
     const body = msg["body"];
     if (typeof body !== "string") {
       return new Response("invalid body", { status: 400 });
     }
-
+    const type = msg["type"];
+    if (typeof type !== "string") {
+      return new Response("invalid type", { status: 400 });
+    }
     const channel = new BroadcastChannel("chat");
 
     const message: Message = {
@@ -31,7 +45,10 @@ serve({
       ts: new Date().toISOString(),
       user,
       body,
+      type,
     };
+
+    user.ts = new Date().getTime();
 
     channel.postMessage(message);
     channel.close();
@@ -60,18 +77,3 @@ serve({
   },
   404: () => new Response("not found"),
 });
-
-// async function handleRequest(request: Request) {
-//   const { pathname } = new URL(request.url);
-
-//   if (pathname.startsWith("/style.css")) {
-//     const style = new URL("style.css", import.meta.url);
-//     return fetch(style);
-//   }
-//   const html = new URL("index.html", import.meta.url);
-//   return fetch(html);
-// }
-
-// addEventListener("fetch", (event: FetchEvent) => {
-//   event.respondWith(handleRequest(event.request));
-// });
